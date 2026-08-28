d1c2b8c1676b35cb4ed74667f924729e58f08558
cofirdere <soumyajeetchowdhury222@gmail.com>
2026-08-28 20:31:31 +0530

    Implementing GUI on the way (not finished)
    Added GUI warning before taking a screenshot and improved comments.

--- MyFirstAutomationWithJava.java
+++ MyFirstAutomationWithJava.java
@@ -1,13 +1,15 @@
-//NOTE: made by Cofirdere, Date of creation:2/28/26
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class MyFirstAutomationWithJava {
     private static final Color TEXT_COLOR=Color.WHITE;
     public static void main(String[] args) {
         //IMPORTANT NOTICE!
-        /* I am a bare beginner, I am a programming newbie, I dont know about all these initially as said by the file name! 
+        /* I am a bare beginner, I am a Beginner, I dont know about all these initially as said by the file name! 
         dont ask for hard updates cuz I am still an beginner learning Java programming! It's just my first year or just months
         I want explanations of: Robot class, the entire awt pack, ImageIO.write() function, how it works!      
         */
@@ -15,7 +17,7 @@ public class MyFirstAutomationWithJava {
         //initialize the robot!
         Robot robot= new Robot();
         robot.setAutoDelay(0);
-
+        MakeGUIWarning();
         
         //get the screen size
         Dimension screenSize=Toolkit.getDefaultToolkit().getScreenSize();  //get the length breath of the default monitor screen size
@@ -25,7 +27,7 @@ public class MyFirstAutomationWithJava {
         //capture photo first
         BufferedImage bufferedImage=robot.createScreenCapture(screenRectangle);
 
-        // String picString=System.getProperty("user.home")+"\\Pictures\\Screenshots";
+        
         //to the system paths of all OS        
         String picString=System.getProperty("user.home") + "/Pictures/Screenshots"; 
 
@@ -35,13 +37,26 @@ public class MyFirstAutomationWithJava {
             directory.mkdirs();
         }
 
-        //generate a new filename using timestamp (I want explaination)
+        //This generates the name of the screenshot file with a timestamp to ensure uniqueness. The file is saved in the specified directory in JPEG format. 
         String timeString="Screenshot_" + System.currentTimeMillis() + ".jpg";
         File systemFile=new File(directory, timeString);
-        ImageIO.write(bufferedImage, "jpg", systemFile);     //so its this [the one that stores the pixels, the format, and the output stream]
+        ImageIO.write(bufferedImage, "jpg", systemFile);     
     }    
     catch (Exception e) {
      e.printStackTrace();
     }
  }
+private static void MakeGUIWarning() {
+    JFrame frame=new JFrame();
+    frame.setAlwaysOnTop(true);
+    frame.setDefaultCloseOperationI(JFrame.EXIT_ON_CLOSE);
+    frame.setLocationRelativeTo(null);
+    frame.setSize(300,300);
+
+    JPanel pannel=new Jpanel();
+    pannel.setLayout(Jpanel, new BoxLayout(pannel, box.Y_AXIS));
+    pannel.setBorder(BorderFactory.createEmptyBorder(12,12,12,12));
+    pannel.setBackground(new Color(100,100,100));
+
+}
 }
